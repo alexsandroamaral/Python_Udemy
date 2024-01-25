88befaea5076667ac6b577f4dd433d547a77b621
Alexsandro Amaral <alexsandroamaral@outlook.com>
2024-01-25 18:29:07 -0400

    Aula12

---
+++ 29_aula12.py
@@ -0,0 +1,9 @@
+nome = 'Alexsandro Amaral'
+altura = 1.66
+peso = 80
+imc = (peso / (altura * altura))
+print( 40 * "---") # Design
+print('')
+print(nome,'tem',altura,', pesa', peso,'KG','e seu IMC é:',imc)
+print('')
+print( 40 * "---") # Design
