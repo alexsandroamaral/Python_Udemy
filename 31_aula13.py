62ff1f2c94cb7aa4bf10a62836ef0f24ee004eb7
Alexsandro Amaral <alexsandroamaral@outlook.com>
2024-01-25 18:45:55 -0400

    Aula13

---
+++ 31_aula13.py
@@ -0,0 +1,13 @@
+nome = 'Alexsandro Amaral'
+altura = 1.66
+peso = 80
+imc = (peso / (altura * altura))
+print( 40 * "---") # Design
+print('')
+print(f'{nome} tem {altura} cm de altura e pesa {peso} kg e seu imc: {imc:.2f} ')
+#print(f'{nome}'tem'{altura}' pesa' {peso}'KG''e seu IMC é:'{imc}')
+print
+print('')
+print( 40 * "---") # Design
+
+#   :.2f = Padrão :.(2) é sobre quantidade casa decimais após o ponto.
